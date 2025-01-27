4adc4200f808bca130912a41dbed6ca1ec4356a0
marekboodt <marek.boodt@hotmail.com>
2025-01-27 17:55:34 +0100

    Update python-sca.sh

--- scripts/01-SCA/python-sca.sh
+++ scripts/01-SCA/python-sca.sh
@@ -4,5 +4,5 @@ echo "Running Python dependency scan..."
 cd "$PROJECT_DIR"
 pip install pip-audit
 pip-audit
-echo "222 Running Python dependency scan...
+echo "222 Running Python dependency scan..."
 pip-audit --output json
