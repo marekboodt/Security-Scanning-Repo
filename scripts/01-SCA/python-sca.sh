9e3c6b818bbd6ebe316d5575a69b7357aee07690
marekboodt <marek.boodt@hotmail.com>
2025-01-27 18:04:31 +0100

    Update python-sca.sh

--- scripts/01-SCA/python-sca.sh
+++ scripts/01-SCA/python-sca.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 PROJECT_DIR=$1
+
+# Generate a dynamic filename for the JSON output
+PROJECT_NAME=$(basename "$PROJECT_DIR") # Get the last part of the project_dir path
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+OUTPUT_FILE="${PROJECT_NAME}.sca.${TIMESTAMP}.json"
+
 echo "Running Python dependency scan..."
 cd "$PROJECT_DIR"
+
 pip install pip-audit
-pip-audit --output json
+pip-audit --output json > "$OUTPUT_FILE"
 echo "Dependency scan completed. Results saved to pip-audit.json."
 
+# Print the filename for debugging purposes
+echo "::set-output name=output_file::$PROJECT_DIR/$OUTPUT_FILE"
