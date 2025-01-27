7309b577078814875dcf1b4c24163412b6f59d64
marekboodt <marek.boodt@hotmail.com>
2025-01-27 20:15:28 +0100

    Update python-sca.sh

--- scripts/01-SCA/python-sca.sh
+++ scripts/01-SCA/python-sca.sh
@@ -12,6 +12,9 @@ OUTPUT_FILE="${REPO_NAME}-SCA-SCAN-${TIMESTAMP}.txt"
 echo "Running Python dependency scan for project: $REPO_NAME ..."
 cd "$PROJECT_DIR"
 
+######################
+# PIP-AUDIT SCANNING #
+######################
 # Install pip-audit
 pip install pip-audit
 
@@ -26,3 +29,8 @@ echo "Dependency scan completed. Results saved to $GITHUB_WORKSPACE/$OUTPUT_FILE
 echo "OUTPUT_FILE=$GITHUB_WORKSPACE/$OUTPUT_FILE" >> $GITHUB_ENV
 # Save the filename only to the environment file
 echo "OUTPUT_FILENAME=$OUTPUT_FILE" >> $GITHUB_ENV
+
+###################
+# SAFETY SCANNING #
+###################
+
