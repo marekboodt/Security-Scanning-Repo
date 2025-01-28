8a9bb856ef0ec046b87c8255c30a735da87b466f
marekboodt <marek.boodt@hotmail.com>
2025-01-28 15:04:16 +0100

    Update python-sca.sh

--- scripts/01-SCA/python-sca.sh
+++ scripts/01-SCA/python-sca.sh
@@ -25,8 +25,8 @@ echo "Starting PIP-AUDIT scan..."
 pip install pip-audit
 
 # pip-audit -f json -o $GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE        # Json Output
-# pip-audit -o $GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE                  # Normal Text Output
-# echo "Dependency scan completed. Results saved to $GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE ."
+ pip-audit -o $GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE                  # Normal Text Output
+ echo "Dependency scan completed. Results saved to $GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE ."
 pip-audit -o "$SCAN_RESULTS_DIR/$PIP_AUDIT_OUTPUT_FILE"
 
 ###################
@@ -36,19 +36,19 @@ echo "Starting SAFETY scan..."
 # Install safety
 pip install safety
 
-# safety check --full-report > "$GITHUB_WORKSPACE/$SAFETY_OUTPUT_FILE"   # Run safety check and save the output
-# echo "SAFETY scan completed. Results saved to $GITHUB_WORKSPACE/$SAFETY_OUTPUT_FILE."
-safety check --full-report > "$SCAN_RESULTS_DIR/$SAFETY_OUTPUT_FILE"
+ safety check --full-report > "$GITHUB_WORKSPACE/$SAFETY_OUTPUT_FILE"   # Run safety check and save the output
+ echo "SAFETY scan completed. Results saved to $GITHUB_WORKSPACE/$SAFETY_OUTPUT_FILE."
+# safety check --full-report > "$SCAN_RESULTS_DIR/$SAFETY_OUTPUT_FILE"
 
 ##################################
 # Save file paths to environment #
 ##################################
 # Save the PIP Audit output file path to an environment file || 1 = file Location,  1 File = Name
-# echo "PIP_AUDIT_OUTPUT_FILE=$GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE" >> $GITHUB_ENV
-# echo "PIP_AUDIT_FILENAME=$PIP_AUDIT_OUTPUT_FILE" >> $GITHUB_ENV
+ echo "PIP_AUDIT_OUTPUT_FILE=$GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE" >> $GITHUB_ENV
+ echo "PIP_AUDIT_FILENAME=$PIP_AUDIT_OUTPUT_FILE" >> $GITHUB_ENV
 
 # Save the SAFETY output file path to an environment file || 1 = file Location,  1 File = Name
-# echo "SAFETY_OUTPUT_FILE=$GITHUB_WORKSPACE/$SAFETY_OUTPUT_FILE" >> $GITHUB_ENV
-# echo "SAFETY_FILENAME=$SAFETY_OUTPUT_FILE" >> $GITHUB_ENV
+ echo "SAFETY_OUTPUT_FILE=$GITHUB_WORKSPACE/$SAFETY_OUTPUT_FILE" >> $GITHUB_ENV
+ echo "SAFETY_FILENAME=$SAFETY_OUTPUT_FILE" >> $GITHUB_ENV
 
-echo "SCAN_RESULTS_DIR=$SCAN_RESULTS_DIR" >> $GITHUB_ENV
+# echo "SCAN_RESULTS_DIR=$SCAN_RESULTS_DIR" >> $GITHUB_ENV
