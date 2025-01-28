c4c3aaa7a94322c79d00d066d794e4840a77afba
marekboodt <marek.boodt@hotmail.com>
2025-01-28 15:49:23 +0100

    Update python-sca.sh

--- scripts/01-SCA/python-sca.sh
+++ scripts/01-SCA/python-sca.sh
@@ -42,6 +42,7 @@ echo "Starting SAFETY scan..."
 # Install safety
 pip install safety
 
+pwd
 safety scan --help
 
 safety scan --json > "$SCAN_RESULTS_DIR/$SAFETY_OUTPUT_FILE"   # Run safety check and save the output as JSON
