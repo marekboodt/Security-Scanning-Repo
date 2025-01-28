0c108dfb5d3477d5b100be92480e3e1fe70c3233
marekboodt <marek.boodt@hotmail.com>
2025-01-28 15:34:52 +0100

    Update python-sca.sh

--- scripts/01-SCA/python-sca.sh
+++ scripts/01-SCA/python-sca.sh
@@ -42,8 +42,8 @@ echo "Starting SAFETY scan..."
 # Install safety
 pip install safety
 
-safety check --output json > "$SCAN_RESULTS_DIR/$SAFETY_OUTPUT_FILE"   # Run safety check and save the output as JSON
-# safety check --full-report > "$SCAN_RESULTS_DIR/$SAFETY_OUTPUT_FILE"   # Full human-readable report
+safety scan --output json > "$SCAN_RESULTS_DIR/$SAFETY_OUTPUT_FILE"   # Run safety check and save the output as JSON
+# safety scan --full-report > "$SCAN_RESULTS_DIR/$SAFETY_OUTPUT_FILE"   # Full human-readable report
 echo "SAFETY scan completed. Results saved to $SCAN_RESULTS_DIR/$SAFETY_OUTPUT_FILE."
  
 #echo "SAFETY scan completed. Results saved to $GITHUB_WORKSPACE/$SAFETY_OUTPUT_FILE."
