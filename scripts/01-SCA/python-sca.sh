5966de78a03953459e120e25ff7c7fc1ec93cc2c
marekboodt <marek.boodt@hotmail.com>
2025-01-28 13:20:22 +0100

    Update python-sca.sh

--- scripts/01-SCA/python-sca.sh
+++ scripts/01-SCA/python-sca.sh
@@ -5,10 +5,10 @@ PROJECT_DIR=$1
 REPO_NAME=$(basename "$GITHUB_REPOSITORY")
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
-#OUTPUT_FILE="${REPO_NAME}-SCA-SCAN-${TIMESTAMP}.json"            # JSON Output
-PIP_AUDIT_OUTPUT_FILE="${REPO_NAME}-SCA-SCAN-${TIMESTAMP}.txt"              # Text Output / Normal Output
-
-SAFETY_OUTPUT_FILE="${REPO_NAME}-SAFETY-SCAN-${TIMESTAMP}.json"   # JSON Output for SAFETY
+# Set the output filename extension 
+PIP_AUDIT_OUTPUT_FILE="${REPO_NAME}-SCA-SCAN-${TIMESTAMP}.txt"          # Text Output / Normal Output (Pip Audit)
+  #PIP_AUDIT_OUTPUT_FILEE="${REPO_NAME}-SCA-SCAN-${TIMESTAMP}.json"     # JSON Output (Pip Audit)
+SAFETY_OUTPUT_FILE="${REPO_NAME}-SAFETY-SCAN-${TIMESTAMP}.json"         # JSON Output for SAFETY
 
 echo "Running Python dependency scan for project: $REPO_NAME ..."
 cd "$PROJECT_DIR"
@@ -20,31 +20,23 @@ echo "Starting PIP-AUDIT scan..."
 # Install pip-audit
 pip install pip-audit
 
-# Json Output
-# pip-audit -f json -o $GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE
-# Normal Text Output
-pip-audit -o $GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE
+# pip-audit -f json -o $GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE        # Json Output
+pip-audit -o $GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE                  # Normal Text Output
 echo "Dependency scan completed. Results saved to $GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE ."
 
-######################
+###################
 # SAFETY SCANNING #
-######################
+###################
 echo "Starting SAFETY scan..."
 # Install safety
 pip install safety
 
-# Run safety check and save the output
-safety check --full-report > "$GITHUB_WORKSPACE/$SAFETY_OUTPUT_FILE"
+safety check --full-report > "$GITHUB_WORKSPACE/$SAFETY_OUTPUT_FILE"   # Run safety check and save the output
 echo "SAFETY scan completed. Results saved to $GITHUB_WORKSPACE/$SAFETY_OUTPUT_FILE."
 
 ##################################
 # Save file paths to environment #
 ##################################
-# Save the output file path to an environment file
-# echo "OUTPUT_FILE=$GITHUB_WORKSPACE/$OUTPUT_FILE" >> $GITHUB_ENV
-# Save the filename only to the environment file
-# echo "OUTPUT_FILENAME=$OUTPUT_FILE" >> $GITHUB_ENV
-
 # Save the PIP Audit output file path to an environment file || 1 = file Location,  1 File = Name
 echo "PIP_AUDIT_OUTPUT_FILE=$GITHUB_WORKSPACE/$PIP_AUDIT_OUTPUT_FILE" >> $GITHUB_ENV
 echo "PIP_AUDIT_FILENAME=$PIP_AUDIT_OUTPUT_FILE" >> $GITHUB_ENV
